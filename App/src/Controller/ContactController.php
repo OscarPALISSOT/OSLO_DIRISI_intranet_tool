cf4c23639018f24efa573d07c07f5713cec29513
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-01-10 13:16:09 +0100

    nettoyage contact

--- App/src/Controller/ContactController.php
+++ App/src/Controller/ContactController.php
@@ -4,17 +4,23 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
-class HomeController extends AbstractController {
+class ContactController extends AbstractController {
 
     public function __construct(Environment $twig)
     {
         $this->twig = $twig;
     }
 
+
+    /**
+     * @Route ("/Contact", name="contact")
+     * @return Response
+     */
     public function index() : Response{
-        return $this->render('pages/home.html.twig', [
+        return $this->render('pages/contact.html.twig', [
 
         ]);
     }
