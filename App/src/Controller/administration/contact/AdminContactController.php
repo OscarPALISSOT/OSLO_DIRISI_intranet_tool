42949754bc6f1e5a45fe3560703f7d0acc76a599
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-03-12 16:57:51 +0100

    contact

--- App/src/Controller/administration/contact/AdminContactController.php
+++ App/src/Controller/administration/contact/AdminContactController.php
@@ -2,10 +2,19 @@
 
 namespace App\Controller\administration\contact;
 
+use App\Entity\Contact;
+use App\Entity\Contactbdd;
+use App\Entity\ContactCirisi;
+use App\Repository\BasesDeDefenseRepository;
+use App\Repository\CirisiRepository;
 use App\Repository\ContactbddRepository;
 use App\Repository\ContactCirisiRepository;
 use App\Repository\ContactRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use League\Csv\Reader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,12 +24,18 @@ class AdminContactController extends AbstractController
     private ContactRepository $ContactRepository;
     private ContactCirisiRepository $ContactCirisiRepository;
     private ContactbddRepository $contactbddRepository;
+    private ManagerRegistry $doctrine;
+    private CirisiRepository $cirisiRepository;
+    private BasesDeDefenseRepository $basesDeDefenseRepository;
 
-    public function __construct(ContactRepository $ContactRepository, ContactCirisiRepository $ContactCirisiRepository, ContactbddRepository $contactbddRepository)
+    public function __construct(ContactRepository $ContactRepository, ContactCirisiRepository $ContactCirisiRepository, ContactbddRepository $contactbddRepository, ManagerRegistry $doctrine, CirisiRepository $cirisiRepository, BasesDeDefenseRepository $basesDeDefenseRepository)
     {
         $this->ContactRepository = $ContactRepository;
         $this->ContactCirisiRepository = $ContactCirisiRepository;
         $this->contactbddRepository = $contactbddRepository;
+        $this->doctrine = $doctrine;
+        $this->cirisiRepository = $cirisiRepository;
+        $this->basesDeDefenseRepository = $basesDeDefenseRepository;
     }
 
     /**
@@ -42,5 +57,77 @@ class AdminContactController extends AbstractController
             'nbContactCirisi' => count($this->ContactCirisiRepository->findAll()),
             'nbContactBdd' => count($this->contactbddRepository->findAll()),
         ]);
+    }
+
+    /**
+     * @Route ("/Admin/ImportContact", name="importContact")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function importInfo(Request $request) : JsonResponse
+    {
+        $file = $request->files->get('file', 'r');
+
+        if ($file == null){
+            $jsonData = array(
+                'message' => "Erreur, veuillez renseignez un fichier.",
+            );
+        }
+        else{
+            $em = $this->doctrine->getManager();
+            $csv = Reader::createFromPath($file->getRealPath());
+            $csv->setHeaderOffset(0);
+            $result = $csv->getRecords();
+
+            foreach ( $result as $row){
+
+                if ($row['Cirisi'] != ''){
+                    $contact = (new ContactCirisi())
+                        ->setIntituleContact($row['Intitule'])
+                        ->setEmailContact($row['Email'])
+                        ->setNomContact($row['Nom'])
+                        ->setPrenomContact($row['Prenom'])
+                        ->setTelContact($row['TPH'])
+                        ->setIdCirisi(
+                            $this->cirisiRepository->findOneBy([
+                                'cirisi' => $row['Cirisi']
+                            ])
+                        )
+                    ;
+                }
+
+                if ($row['Bdd'] != ''){
+                    $contact = (new Contactbdd())
+                        ->setIntituleContact($row['Intitule'])
+                        ->setEmailContact($row['Email'])
+                        ->setNomContact($row['Nom'])
+                        ->setPrenomContact($row['Prenom'])
+                        ->setTelContact($row['TPH'])
+                        ->setIdBaseDefense(
+                            $this->basesDeDefenseRepository->findOneBy([
+                                'baseDefense' => $row['Cirisi']
+                            ])
+                        )
+                    ;
+                }
+
+                if ($row['Bdd'] == '' and $row['Cirisi'] == ''){
+                    $contact = (new Contact())
+                        ->setIntituleContact($row['Intitule'])
+                        ->setEmailContact($row['Email'])
+                        ->setNomContact($row['Nom'])
+                        ->setPrenomContact($row['Prenom'])
+                        ->setTelContact($row['TPH'])
+                    ;
+                }
+
+            }
+
+            $jsonData = array(
+                'message' => "Importation terminée",
+            );
+        }
+
+        return $this->json($jsonData, 200);
     }
 }
