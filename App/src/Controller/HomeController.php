132e5008c9dfbc2cba5e1cbe06f4588f2191111a
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2022-04-04 15:31:05 +0200

    home page base defense

--- App/src/Controller/HomeController.php
+++ App/src/Controller/HomeController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\BasesDeDefenseRepository;
+use App\Repository\QuartiersRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController {
 
-    public function __construct()
+    private $basesDeDefenseRepository;
+    private $quartiersRepository;
+
+    public function __construct(BasesDeDefenseRepository $basesDeDefenseRepository, QuartiersRepository $quartiersRepository)
     {
+        $this->basesDeDefenseRepository = $basesDeDefenseRepository;
+        $this->quartiersRepository = $quartiersRepository;
     }
 
     public function index() : Response{
         return $this->render('pages/home.html.twig', [
-
+            'Bdds' => $this->basesDeDefenseRepository->findAll(),
+            'Quartiers' => $this->quartiersRepository->findAll(),
         ]);
     }
 }
